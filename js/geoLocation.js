9941c71ab2bca77bc6a2bd03abcfdd909b04f73d
koop4 <dilillo.roberto@gmail.com>
2017-08-01 13:05:01 +0200

    fetch weather from location

--- js/geoLocation.js
+++ js/geoLocation.js
@@ -1,13 +1,26 @@
-getLocation();
 
-function getLocation() {
+var findLocation = new Promise(function(resolve, reject) {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      // display 404 like in this case
+        navigator.geolocation.getCurrentPosition(resolve,cantFind);
     }
+    function cantFind(error) { 
+        if (error.code == error.PERMISSION_DENIED) {
+            reject("you denied me :-(");
+        } 
+        else {
+            reject('err code: '+error.code);
+        }
+    } 
+});
+
+findLocation.then(getMeteo).catch(function(err) { console.log(err) });
+
+function getMeteo(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    fetch('https://fcc-weather-api.glitch.me/api/current?lat='+lat+'&lon='+lng)  
+        .then(function(response) { return response.json(); })
+        .then(function(response) { console.log(response) })
+        .catch(function(err) { console.log(err) });
 }
-function showPosition(position) {
-    var pos = "Latitude: " + position.coords.latitude +  "<br>Longitude: " + position.coords.longitude; 
-    console.log(pos)
-}
+
