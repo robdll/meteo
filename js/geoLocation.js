699f9ec5e02ba2ea4631ff3f24c080a3bde75d02
koop4 <dilillo.roberto@gmail.com>
2017-08-06 22:59:02 +0200

    reorganize function and add fadeIn

--- js/geoLocation.js
+++ js/geoLocation.js
@@ -20,15 +20,14 @@ function getMeteo(location) {
     var lat = location.coords.latitude;
     var lng = location.coords.longitude;
     fetch('https://fcc-weather-api.glitch.me/api/current?lat='+lat+'&lon='+lng)  
-  //  fetch('https://fcc-weather-api.glitch.me/api/current?lat=45.46&lon=9.19')  
         .then(function(response) { return response.json(); })
         .then(function(response) { console.log(response); return response; })
-        .then(addArticle)
+        .then(storeWeatherData)
         .catch(function(err) { console.log(err) });
+    //internal function
 
-    function addArticle(data) {
-        var main, article, paragraph, details;
-        var weather = {
+    function storeWeatherData(data) {
+        window.weather = {
             city: data.name,
             country: data.sys.country,
             temp: data.main.temp,
@@ -42,37 +41,7 @@ function getMeteo(location) {
             description: data.weather[0].description,
             icon: data.weather[0].icon,
         }
-        mainTag = document.getElementsByTagName("MAIN")[0];
-        article = create('ARTICLE');
-        article.classList.add("weather-screen");
-        article.appendChild(create('p', weather.city + ' - ' + weather.country ));
-        article.appendChild(create('p', weather.description));
-        article.appendChild(create('p', getIcon() + weather.temp + getSwitch() ));
         window.hasLocation = true;
-        details = create('details');
-        details.classList.add('noselect');
-        details.appendChild(create('p', 'humidity: '+weather.humidity ));
-        details.appendChild(create('p', 'pressure: '+weather.pressure ));
-        details.appendChild(create('p', 'min: '+weather.temp_min ));
-        details.appendChild(create('p', 'max: '+weather.temp_max ));
-        article.appendChild(details);
-        mainTag.appendChild(article);
-        //internal function
-        function create(tag, html){ 
-            var tag = document.createElement(tag); 
-            tag.classList.add("noselect");
-            if(html) { tag.innerHTML = html };
-            return tag;
-        }
-        function getIcon() {
-            var hotLvl = weather.temp % 6 > 4 ? 4 : weather.temp % 6;
-            hotLvl += 0.5;
-            hotLvl = Math.floor(hotLvl);
-            return '<i class="fa fa-thermometer-'+ hotLvl + '"></i> ';
-        }
-        function getSwitch() {
-           return ' &#8451;' +'<a class="switch">/&#8457;</a>'
-        }
     }
 }
 
--- js/loader.js
+++ js/loader.js
@@ -15,10 +15,49 @@ function waitForLocation() {
         if(window.hasLocation || window.locationError) {
             clearInterval(fakeWriter);
             if(window.hasLocation){
-                loaderMsg.outerHTML = '';
-                delete loaderMsg;
+                loaderMsg.classList.add('hide');
+                setTimeout(function(){
+                    loaderMsg.outerHTML = '';
+                    delete loaderMsg;
+                    createWeatherScreen();
+                },2000)
             }
         }
     }
 
+    function createWeatherScreen(){
+        var weather = window.weather;
+        var main, article, paragraph, details;
+        mainTag = document.getElementsByTagName("MAIN")[0];
+        article = create('ARTICLE');
+        article.classList.add("weather-screen");
+        article.appendChild(create('p', weather.city + ' - ' + weather.country ));
+        article.appendChild(create('p', weather.description));
+        article.appendChild(create('p', getIcon() + weather.temp + getSwitch() ));
+        details = create('details');
+        details.classList.add('noselect');
+        details.appendChild(create('p', 'humidity: '+weather.humidity ));
+        details.appendChild(create('p', 'pressure: '+weather.pressure ));
+        details.appendChild(create('p', 'min: '+weather.temp_min ));
+        details.appendChild(create('p', 'max: '+weather.temp_max ));
+        article.appendChild(details);
+        mainTag.appendChild(article);
+        article.classList.add('show')
+        //internal function
+        function create(tag, html){ 
+            var tag = document.createElement(tag); 
+            tag.classList.add("noselect");
+            if(html) { tag.innerHTML = html };
+            return tag;
+        }
+        function getIcon() {
+            var hotLvl = weather.temp % 6 > 4 ? 4 : weather.temp % 6;
+            hotLvl += 0.5;
+            hotLvl = Math.floor(hotLvl);
+            return '<i class="fa fa-thermometer-'+ hotLvl + '"></i> ';
+        }
+        function getSwitch() {
+           return ' &#8451;' +'<a class="switch">/&#8457;</a>'
+        }
+    }
 }
